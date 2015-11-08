c19de6dd256cc76cc956743d7687d3ec550ee8af
LeXX <spololin@outlook.com>
2015-11-08 22:58:27 +0300

    +

---
+++ Primers_js_jq_macfarland/Примеры/глава07/js1.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#gallery img').each(function () {
+        var imgFile = $(this).attr('src');
+        var preloadImg = new Image();
+        var imgExt = /(\.\w{3,4}$)/;
+        preloadImg.src = imgFile.replace(imgExt, '_h$1');
+        $(this).hover(
+                function () {
+                    $(this).attr('src', preloadImg.src);
+                },
+                function () {
+                    $(this).attr('src', imgFile);
+                }
+        );
+    });
+}); // end ready
